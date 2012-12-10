ce2621ca3a1acd79c094de8d58ee8a382a35e299
Felix Sun <sunfmin@gmail.com>
2012-12-10 16:42:01 +0800

    add group entries

--- api.go
+++ api.go
@@ -12,12 +12,12 @@ type UserService interface {
 	// CreatePostWithTask() (err error)
 	// UpdatePostWithTask() (err error)
 	// MyFeedEntries(entryType string, before time.Time, limit int) (r []*Entry, err error)
-	// GroupEntries(groupId string, entryType string, before time.Time, limit int) (g *Group, r []*Entry, err error)
+	GroupEntries(groupId string, entryType string, before time.Time, limit int) (g *Group, r []*Entry, err error)
 	// UserEntries(userId string, entryType string, before time.Time, limit int) (u *User, r []*Entry, err error)
 	// LoadEntry(groupId string, entryId string) (g *Group, r *Entry, err error)
 	GetWatchList(before time.Time, limit int) (r *WatchList, err error)
-	AddToWatchList(entryId string, groupId string) (IsAdded bool, err error)
-	StopWatching(entryId string, groupId string) (IsAdded bool, err error)
+	AddToWatchList(entryId string, groupId string) (added bool, err error)
+	StopWatching(entryId string, groupId string) (stopped bool, err error)
 	ReadWatching(entryId string, groupId string) (err error)
 }
 
