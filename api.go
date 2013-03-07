6a983b50423c19a6032a3ba2feba77b2ae62bdd2
athom <yeerkunth@gmail.com>
2013-03-07 23:56:30 +0800

    update get sharign inviation API

--- api.go
+++ api.go
@@ -14,7 +14,7 @@ type NoAuthUserService interface {
 	CancelChangingEmail(token string) (err error)
 	ChangeEmail(token string) (activationToken string, err error)
 	PrepareChangeEmail(memberId string, newEmail string) (r *EmailChanger, validated *govalidations.Validated, err error)
-	GetSharingInviation(sharingInviationToken string) (r *SharingInvitationItem, err error)
+	GetSharingInviation(sharingInviationToken string, memberId string) (r *SharingInvitationItem, err error)
 	ChangeEmailToAcceptSharing(token string, newEmail string) (validated *govalidations.Validated, err error)
 
 	ResponseSharingRequest(token string, fromOrgId string, fromUserId string, forSharingOrgId string, groupId string) (prefixURL string, validated *govalidations.Validated, err error)
