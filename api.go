d9973197c593d12ed2c72e299203fce539758df4
kobeld <kobeld@163.com>
2013-03-29 15:53:26 +0800

    Add get sharing invitation request method

--- api.go
+++ api.go
@@ -15,6 +15,7 @@ type NoAuthUserService interface {
 	ChangeEmail(token string) (activationToken string, err error)
 	PrepareChangeEmail(memberId string, newEmail string) (r *EmailChanger, validated *govalidations.Validated, err error)
 	GetSharingInviation(sharingInviationToken string, memberId string) (r *SharingInvitationItem, err error)
+
 	ChangeEmailToAcceptSharing(token string, newEmail string) (validated *govalidations.Validated, err error)
 
 	ResponseSharingRequest(token string, fromOrgId string, fromUserId string, forSharingOrgId string, groupId string) (prefixURL string, validated *govalidations.Validated, err error)
@@ -32,6 +33,7 @@ type NoAuthUserService interface {
 type AuthMemberService interface {
 	GetAbandonUserInfo(organizationId string, memberId string) (info *AbandonUserInfo, err error)
 	SwitchOrganization(orgId string) (err error)
+	GetSharingInviationByToken(sharingInviationToken string) (r *SharingInvitationItem, err error)
 }
 
 type AuthUserService interface {
