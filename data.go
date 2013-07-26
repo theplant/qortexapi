dcd68c463f5d23d2264dd2ea35ac8eddf27f5cb3
athom <athom@126.com>
2013-07-26 15:06:55 +0800

    rebame CSV value for user status

--- data.go
+++ data.go
@@ -725,10 +725,10 @@ type OrgUnreadInfo struct {
 }
 
 const (
-	MS_RECEIVE_JOIN_ORG_INVITATION        = "receive join org inviation but not activate"
-	MS_RECEIVE_SHARED_GROUP_INVITATION    = "receive shared group inviation but not activate"
-	MS_APPROVED                           = "approve access but not setup account"
-	MS_SETUP_ACCOUNT                      = "setup account but not create organization"
+	MS_RECEIVE_JOIN_ORG_INVITATION        = "InvitedNotSignedUpNudgeToUser"
+	MS_RECEIVE_SHARED_GROUP_INVITATION    = "InvitedToSharedGroupNotSignedUpNudge"
+	MS_APPROVED                           = "ReceivedBetaInvitationNotSignedUpNudge"
+	MS_SETUP_ACCOUNT                      = "JustSignedUpNudgeToAdmin"
 	MS_ACTIVATE_BY_JOIN_ORG_INVITATION    = "only activate account from join org inviation but not join the org"
 	MS_ACTIVATE_BY_SHARE_GROUP_INVITATION = "only activate account from shared group inviation but not create any organization"
 	MS_CREATE_ORG                         = "create first organization after setup account"
