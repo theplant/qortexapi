1392d0883efbf0561b7b38e320a74ed88b58abd1
athom <athom@126.com>
2013-08-06 17:26:06 +0800

    update MailChimpStatus

--- data.go
+++ data.go
@@ -729,14 +729,16 @@ const (
 	MS_RECEIVE_JOIN_ORG_INVITATION        = "InvitedNotSignedUpNudgeToUser"
 	MS_RECEIVE_SHARED_GROUP_INVITATION    = "InvitedToSharedGroupNotSignedUpNudge"
 	MS_APPROVED                           = "ReceivedBetaInvitationNotSignedUpNudge"
-	MS_SETUP_ACCOUNT                      = "JustSignedUpDirectlyNudgeToAdmin "
-	MS_ACTIVATE_BY_JOIN_ORG_INVITATION    = "JustSignedUpViaInvitedNudgeToAdmin "
+	MS_SETUP_ACCOUNT                      = "JustSignedUpDirectlyNudgeToAdmin"
+	MS_ACTIVATE_BY_JOIN_ORG_INVITATION    = "JustSignedUpViaInvitedNudgeToAdmin"
 	MS_ACTIVATE_BY_SHARE_GROUP_INVITATION = "JustSignedUpViaSharedGroupNudgeToAdmin"
-	MS_CREATE_ORG                         = "create first organization after setup account"
-	MS_JOIN_ORG                           = "join first organization after activate account"
-	MS_INACTIVE_DURING_BETA               = "InactiveDuringFreeTrialNudgeToAdmin"
-	MS_INACTIVE_NEAR_BETA                 = "InactiveNearTrialEndNudgeToAdmin"
-	MS_ACTIVE                             = "ActiveNearTrialEndNudgeToAdmin"
+	MS_CREATE_ORG                         = "JustCreatedOrgNudgeToAdmin"
+	MS_JOIN_ORG                           = "JustJoinedOrgNudgeToAdmin"
+	MS_CREATE_ORG_VIA_SHARE_GROUP         = "JustCreatedOrgViaSharedGroupNudgeToAdmin"
+	MS_INACTIVE_DURING_TRIAL              = "InactiveDuringFreeTrialNudgeToAdmin"
+	MS_INACTIVE_AT_TRIAL                = "InactiveAtTrialEndNudgeToAdmin"
+	MS_ACTIVE_AT_TRIAL                    = "ActiveAtTrialEndNudgeToAdmin"
+	MS_ACTIVE_NEAR_TRIAL                  = "ActiveNearTrialEndNudgeToAdmin"
 )
 
 type MarketableMemberInfo struct {
