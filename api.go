a0b43bc8aa420c5e23c08b9e051f26214119f37a
Azuma Fan <azuma@theplant.jp>
2013-08-09 17:20:57 +0800

    add locale to getsession

--- api.go
+++ api.go
@@ -6,7 +6,7 @@ import (
 )
 
 type PublicService interface {
-	GetSession(email string, password string) (session string, err error)
+	GetSession(email string, password string, locale string) (session string, err error)
 	GetAuthUserService(session string) (authUserService AuthUserService, err error)
 	GetAuthorizedAdmin(session string) (apiEmbedUser EmbedUser, err error)
 	GetAuthAdminService(session string) (authAdminService AuthAdminService, err error)
