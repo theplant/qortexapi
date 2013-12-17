99a2fc3bec91ff2145576a97b641417517484dee
Azuma Fan <azuma@theplant.jp>
2013-12-17 15:49:12 +0800

    Add NeedPayError

--- errors.go
+++ errors.go
@@ -47,4 +47,5 @@ var (
 	HighFrequencyError             = errors.New("High frequency requests")
 	SaveTranscriptionError         = errors.New("Can not save transcription")
 	ClaimTaskError                 = errors.New("Task was already cliamed by others")
+	NeedPayError                   = errors.New("Qortex need to pay")
 )
