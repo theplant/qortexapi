3bf0949dbc64a85cda987511eb2b5f82bf8737ff
bom_d_van <bom.d.van@gmail.com>
2014-02-21 16:25:22 +0800

    group-collections: add data GroupCollection

--- data.go
+++ data.go
@@ -1105,3 +1105,8 @@ type KnowledgeOverview struct {
 	IsHidePresentationTip   bool              `json:",omitempty"` //just for reuse the mannual translation form
 	Id                      string            `json:",omitempty"` //just for reuse the mannual translation form
 }
+
+type GroupCollection struct {
+	Id   string
+	Name string
+}
